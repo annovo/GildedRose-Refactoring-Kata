71f0231d08c9f2d09327b4d7d3bfe78c1308e531
Anastasiia Novoselova <ananas.nasya@gmail.com>
2022-03-10 16:48:03 -0800

    second refactoring

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -5,44 +5,125 @@ class Item {
     this.quality = quality;
   }
 }
+const MAX_QUALITY_VALUE = 50;
+const MIN_QUALITY_VALUE = 0;
+const LEGENDARY_ITEM_VALUE = 80;
+
+class DateRange {
+  constructor(lowerBound, upperBound) {
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
+  }
+}
+
+class NonUpdatableItem {
+  constructor(value) {
+    this.value = value;
+  }
+
+  updateItem(item) {
+    item.quality = this.value;
+  }
+}
+
+class UpdatableItem extends NonUpdatableItem {
+  constructor(minVal, maxVal, dateRange, valUpdateRange) {
+    super();
+    this.maxVal = maxVal;
+    this.minVal = minVal;
+    this.dateRange = [...dateRange];
+    this.valUpdateRange = [...valUpdateRange];
+  }
+
+  updateItem(item) {
+    item.sellIn--;
+    for (let i = 0; i < this.dateRange.length; i++) {
+      if (
+        item.sellIn > this.dateRange[i].lowerBound &&
+        item.sellIn <= this.dateRange[i].upperBound
+      ) {
+        item.quality += this.valUpdateRange[i];
+        break;
+      }
+    }
+
+    if (item.quality < this.minVal) item.quality = this.minVal;
+    else if (item.quality > this.maxVal) item.quality = this.maxVal;
+  }
+}
+
+const itemToParams = {
+  "Aged Brie": {
+    minVal: MIN_QUALITY_VALUE,
+    maxVal: MAX_QUALITY_VALUE,
+    dateRange: [
+      new DateRange(Number.MIN_SAFE_INTEGER, -1),
+      new DateRange(-1, Number.MAX_SAFE_INTEGER),
+    ],
+    valRange: function () {
+      return [2, 1];
+    },
+  },
+  "Backstage passes": {
+    minVal: MIN_QUALITY_VALUE,
+    maxVal: MAX_QUALITY_VALUE,
+    dateRange: [
+      new DateRange(Number.MIN_SAFE_INTEGER, -1),
+      new DateRange(-1, 5),
+      new DateRange(5, 10),
+      new DateRange(10, Number.MAX_SAFE_INTEGER),
+    ],
+    valRange: function () {
+      return [-this.maxVal, 3, 2, 1];
+    },
+  },
+  default: {
+    minVal: MIN_QUALITY_VALUE,
+    maxVal: MAX_QUALITY_VALUE,
+    dateRange: [
+      new DateRange(Number.MIN_SAFE_INTEGER, -1),
+      new DateRange(-1, Number.MAX_SAFE_INTEGER),
+    ],
+    valRange: function () {
+      return [-2, -1];
+    },
+  },
+};
 
 class Shop {
   constructor(items = []) {
     this.items = items;
   }
 
-  addQuality(item, quality) {
-    item.quality += quality;
-    if (item.quality < 0) item.quality = 0;
-    else if (item.quality > 50) item.quality = 50;
+  getKey(name) {
+    for (const key in itemToParams) {
+      if (key === "default") continue;
+      if (name.startsWith(key)) return itemToParams[key];
+    }
+    return itemToParams.default;
+  }
+
+  categorize(name) {
+    if (name.startsWith("Sulfuras"))
+      return new NonUpdatableItem(LEGENDARY_ITEM_VALUE);
+
+    let itemParams = itemToParams["default"];
+    for (const key in itemToParams) {
+      if (key === "default") continue;
+      if (name.startsWith(key)) itemParams = itemToParams[key];
+    }
+    return new UpdatableItem(
+      itemParams.minVal,
+      itemParams.maxVal,
+      itemParams.dateRange,
+      itemParams.valRange()
+    );
   }
 
   updateQuality() {
     for (let item of this.items) {
-      item.sellIn--;
-      if (item.quality < 0) item.quality = 0;
-
-      switch (item.name) {
-        case "Sulfuras, Hand of Ragnaros":
-          item.quality = 80;
-          item.sellIn++;
-          break;
-        case "Aged Brie":
-          this.addQuality(item, item.sellIn < 0 ? 2 : 1);
-          break;
-        case "Backstage passes to a TAFKAL80ETC concert":
-          let newQuality;
-          if (item.sellIn > 10) newQuality = 1;
-          else if (item.sellIn > 5) newQuality = 2;
-          else if (item.sellIn >= 0) newQuality = 3;
-          else newQuality = -item.quality;
-
-          this.addQuality(item, newQuality);
-          break;
-        default:
-          this.addQuality(item, item.sellIn < 0 ? -2 : -1);
-          break;
-      }
+      let updatedItem = this.categorize(item.name);
+      updatedItem.updateItem(item);
     }
 
     return this.items;
